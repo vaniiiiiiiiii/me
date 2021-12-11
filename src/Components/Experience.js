c8e3bd714be3236c1055ac2594f0fcd0052de412
Jovanie Limbagan <limbagan.j@sprobe.com>
2021-12-11 14:29:43 +0800

    Update experience section

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,149 +1,163 @@
 function Experience() {
-    return(
-        <div>
-            <section className="colorlib-experience" data-section="experience">
-                <div className="colorlib-narrow-content">
-                    <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-                        <span className="heading-meta">Experience</span>
-                        <h2 className="colorlib-heading animate-box">Work Experience</h2>
-                    </div>
-                    </div>
-                    <div className="row">
-                    <div className="col-md-12">
-                        <div className="timeline-centered">
-                        <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-                            <div className="timeline-entry-inner">
-                            <div className="timeline-icon color-1">
-                                <i className="icon-pen2" />
-                            </div>
-                            <div className="timeline-label">
-                                <h2><a href="#">Sr. Software Engineer at Sprobe Inc.</a> <span>July 2019 - August 2019, March 2020 - January 2021</span></h2>
-                                <ul>
-                                    <li>Leads project development</li>
-                                    <li>Enhances client's product</li>
-                                    <li>Develop new functions in C#.Net</li>
-                                    <li>Collaborates with client's Japanese Software Engineers</li>
-                                </ul>
-                            </div>
-                            </div>
-                        </article>
-                        <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-                            <div className="timeline-entry-inner">
-                            <div className="timeline-icon color-2">
-                                <i className="icon-pen2" />
-                            </div>
-                            <div className="timeline-label">
-                                <h2><a href="#">Software Engineer at Delta Co., Ltd.</a> <span>September 2019 - February 2020</span></h2>
-                                <ul>
-                                    <li>Enhances client's product</li>
-                                    <li>Develop new functions in C#.Net</li>
-                                    <li>Collaborates with client's Japanese Software Engineers</li>
-                                </ul>
-                            </div>
-                            </div>
-                        </article>
-                        <article className="timeline-entry animate-box" data-animate-effect="fadeInRight">
-                            <div className="timeline-entry-inner">
-                            <div className="timeline-icon color-3">
-                                <i className="icon-pen2" />
-                            </div>
-                            <div className="timeline-label">
-                                <h2><a href="#">Software Engineer at Sprobe Inc.</a> <span>April 2019 - June 2019</span></h2>
-                                <ul>
-                                    <li>Responsible for creating ERD and Sequence Diagrams</li>
-                                    <li>Responsible for creating systems based from client’s specifications</li>
-                                    <li>Researches and decides on technologies best suited for systems</li>
-                                </ul>
-                            </div>
-                            </div>
-                        </article>
-                        <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-                            <div className="timeline-entry-inner">
-                            <div className="timeline-icon color-4">
-                                <i className="icon-pen2" />
-                            </div>
-                            <div className="timeline-label">
-                                <h2><a href="#">Associate Software Engineer at Sprobe Inc.</a> <span>January 2018 - March 2019</span></h2>
-                                <ul>
-                                    <li>Responsible for programming related tasks such as coding, testing and implementation</li>
-                                </ul>
-                            </div>
-                            </div>
-                        </article>
-                        <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
-                            <div className="timeline-entry-inner">
-                            <div className="timeline-icon color-5">
-                                <i className="icon-pen2" />
-                            </div>
-                            <div className="timeline-label">
-                                <h2><a href="#">Programmer at Sprobe Inc.</a> <span>April 2016 - December 2017</span></h2>
-                                <ul>
-                                    <li>Write programs in a variety of computer languages, such as PHP, Java and C# .Net</li>
-                                    <li>Update and expand existing programs</li>
-                                    <li>Debug programs by testing and fixing errors</li>
-                                    <li>Build and use computer-assisted software engineering (CASE) tools to automate the writing of some code</li>
-                                    <li>Use code libraries, which are collections of independent lines of code, to simplify the writing</li>
-                                </ul>
-                            </div>
-                            </div>
-                        </article>
-                        <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
-                            <div className="timeline-entry-inner">
-                            <div className="timeline-icon color-5">
-                                <i className="icon-pen2" />
-                            </div>
-                            <div className="timeline-label">
-                                <h2><a href="#">Programmer at PTMS Inc.</a> <span>November 2014 - March 2016</span></h2>
-                                <ul>
-                                    <li>Researching and examining current systems and consulting users</li>
-                                    <li>Liaising with colleagues such as systems analyst and designers</li>
-                                    <li>Writing software codes with CakePHP Framework</li>
-                                    <li>Testing and Modifying systems to ensure that they operate reliably</li>
-                                    <li>Fault finding and fixing</li>
-                                </ul>
-                            </div>
-                            </div>
-                        </article>
-                        <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-                            <div className="timeline-entry-inner">
-                            <div className="timeline-icon color-6">
-                                <i className="icon-pen2" />
-                            </div>
-                            <div className="timeline-label">
-                                <h2><a href="#">Customer Service Associate at Tech Mahindra</a> <span>September 2014 - October 2014</span></h2>
-                                <ul>
-                                    <li>Responsible for assisting customers with any inquiries while ensuring the highest standards of customer service are met</li>
-                                </ul>
-                            </div>
-                            </div>
-                        </article>
-                        <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-                            <div className="timeline-entry-inner">
-                            <div className="timeline-icon color-7">
-                                <i className="icon-pen2" />
-                            </div>
-                            <div className="timeline-label">
-                                <h2><a href="#">Student Assistant at University of Cebu</a> <span>April 2008 - April 2014</span></h2>
-                                <ul>
-                                    <li>Assists the Building Maintenance Supervisor/Campus Engineer and staffs in the office</li>
-                                </ul>
-                            </div>
-                            </div>
-                        </article>
-                        <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
-                            <div className="timeline-entry-inner">
-                            <div className="timeline-icon color-none">
-                            </div>
-                            </div>
-                        </article>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-    );
+	return (
+		<div>
+			<section className="colorlib-experience" data-section="experience">
+				<div className="colorlib-narrow-content">
+					<div className="row">
+						<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+							<span className="heading-meta">Experience</span>
+							<h2 className="colorlib-heading animate-box">Work Experience</h2>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-md-12">
+							<div className="timeline-centered">
+								<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+									<div className="timeline-entry-inner">
+										<div className="timeline-icon color-1">
+											<i className="icon-pen2" />
+										</div>
+										<div className="timeline-label">
+											<h2><a href="#">Frontend Engineer at LIG</a> <span>March 2021 - Present</span></h2>
+											<ul>
+												<li>Leads project development</li>
+												<li>Plan, build, and implement user interface systems of websites.</li>
+											</ul>
+										</div>
+									</div>
+								</article>
+								<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+									<div className="timeline-entry-inner">
+										<div className="timeline-icon color-1">
+											<i className="icon-pen2" />
+										</div>
+										<div className="timeline-label">
+											<h2><a href="#">Sr. Software Engineer at Sprobe Inc.</a> <span>July 2019 - August 2019, March 2020 - January 2021</span></h2>
+											<ul>
+												<li>Leads project development</li>
+												<li>Enhances client's product</li>
+												<li>Develop new functions in C#.Net</li>
+												<li>Collaborates with client's Japanese Software Engineers</li>
+											</ul>
+										</div>
+									</div>
+								</article>
+								<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+									<div className="timeline-entry-inner">
+										<div className="timeline-icon color-2">
+											<i className="icon-pen2" />
+										</div>
+										<div className="timeline-label">
+											<h2><a href="#">Software Engineer at Delta Co., Ltd.</a> <span>September 2019 - February 2020</span></h2>
+											<ul>
+												<li>Enhances client's product</li>
+												<li>Develop new functions in C#.Net</li>
+												<li>Collaborates with client's Japanese Software Engineers</li>
+											</ul>
+										</div>
+									</div>
+								</article>
+								<article className="timeline-entry animate-box" data-animate-effect="fadeInRight">
+									<div className="timeline-entry-inner">
+										<div className="timeline-icon color-3">
+											<i className="icon-pen2" />
+										</div>
+										<div className="timeline-label">
+											<h2><a href="#">Software Engineer at Sprobe Inc.</a> <span>April 2019 - June 2019</span></h2>
+											<ul>
+												<li>Responsible for creating ERD and Sequence Diagrams</li>
+												<li>Responsible for creating systems based from client’s specifications</li>
+												<li>Researches and decides on technologies best suited for systems</li>
+											</ul>
+										</div>
+									</div>
+								</article>
+								<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+									<div className="timeline-entry-inner">
+										<div className="timeline-icon color-4">
+											<i className="icon-pen2" />
+										</div>
+										<div className="timeline-label">
+											<h2><a href="#">Associate Software Engineer at Sprobe Inc.</a> <span>January 2018 - March 2019</span></h2>
+											<ul>
+												<li>Responsible for programming related tasks such as coding, testing and implementation</li>
+											</ul>
+										</div>
+									</div>
+								</article>
+								<article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+									<div className="timeline-entry-inner">
+										<div className="timeline-icon color-5">
+											<i className="icon-pen2" />
+										</div>
+										<div className="timeline-label">
+											<h2><a href="#">Programmer at Sprobe Inc.</a> <span>April 2016 - December 2017</span></h2>
+											<ul>
+												<li>Write programs in a variety of computer languages, such as PHP, Java and C# .Net</li>
+												<li>Update and expand existing programs</li>
+												<li>Debug programs by testing and fixing errors</li>
+												<li>Build and use computer-assisted software engineering (CASE) tools to automate the writing of some code</li>
+												<li>Use code libraries, which are collections of independent lines of code, to simplify the writing</li>
+											</ul>
+										</div>
+									</div>
+								</article>
+								<article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+									<div className="timeline-entry-inner">
+										<div className="timeline-icon color-5">
+											<i className="icon-pen2" />
+										</div>
+										<div className="timeline-label">
+											<h2><a href="#">Programmer at PTMS Inc.</a> <span>November 2014 - March 2016</span></h2>
+											<ul>
+												<li>Researching and examining current systems and consulting users</li>
+												<li>Liaising with colleagues such as systems analyst and designers</li>
+												<li>Writing software codes with CakePHP Framework</li>
+												<li>Testing and Modifying systems to ensure that they operate reliably</li>
+												<li>Fault finding and fixing</li>
+											</ul>
+										</div>
+									</div>
+								</article>
+								<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+									<div className="timeline-entry-inner">
+										<div className="timeline-icon color-6">
+											<i className="icon-pen2" />
+										</div>
+										<div className="timeline-label">
+											<h2><a href="#">Customer Service Associate at Tech Mahindra</a> <span>September 2014 - October 2014</span></h2>
+											<ul>
+												<li>Responsible for assisting customers with any inquiries while ensuring the highest standards of customer service are met</li>
+											</ul>
+										</div>
+									</div>
+								</article>
+								<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+									<div className="timeline-entry-inner">
+										<div className="timeline-icon color-7">
+											<i className="icon-pen2" />
+										</div>
+										<div className="timeline-label">
+											<h2><a href="#">Student Assistant at University of Cebu</a> <span>April 2008 - April 2014</span></h2>
+											<ul>
+												<li>Assists the Building Maintenance Supervisor/Campus Engineer and staffs in the office</li>
+											</ul>
+										</div>
+									</div>
+								</article>
+								<article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+									<div className="timeline-entry-inner">
+										<div className="timeline-icon color-none">
+										</div>
+									</div>
+								</article>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
 }
 
 export default Experience;
